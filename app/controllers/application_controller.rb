370c101ed346c85577fab281e11c8e5e96607704
Luis Ibarra <luis9609@gmail.com>
2022-07-18 13:04:14 -0600

    commit tree 7b2e2d1ad52800e44ef5e128fbd3f99e2c28e949

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+
+    def index
+        birds = Bird.all
+        render json: birds
+    end
+
+    def show
+        bird = Bird.find_by(id: params[:id])
+        if bird
+            render json: bird
+        else
+            render json: { error: "Bird not found" }, status: :not_found
+        end
+    end
+    
 end
